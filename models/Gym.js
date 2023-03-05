5e252025b75340b07265a0e45480d9bc5b24d9c5
MyselfDeveloperGit <szyszkakontrast@gmail.com>
2023-03-05 13:49:24 +0100

    Adding Reviews and validation

--- models/Gym.js
+++ models/Gym.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import Review from "./Review.js";
 const Schema = mongoose.Schema;
 
 const GymSchema = new Schema({
@@ -7,6 +8,15 @@ const GymSchema = new Schema({
   description: String,
   location: String,
   image: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+});
+
+GymSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: { $in: doc.reviews },
+    });
+  }
 });
 
 export default mongoose.model("Gym", GymSchema);
